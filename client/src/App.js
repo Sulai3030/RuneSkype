fc361091a6b84b946e9c6f2892e784a30ce31aad
T-Rex <tamhartman@gmail.com>
2018-11-14 20:26:06 -0500

    reset

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react'
-import './App.css';
-import Home from "./Home"
-import Play from './play'
-import { BrowserRouter as Router, Route } from "react-router-dom"
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-
-    render() {
-        return (
-          <Router>
-          <div>
-            <Route exact path ="/" component={Home} />
-            <Route exact path='/play' component={Play} />
-          </div>
-        </Router>
-    
-    
-      );
-      }
- 
-
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
