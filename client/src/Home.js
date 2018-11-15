78fb4cffd8e0f1e16337dc38aae767056aaf33dd
Jroze88 <Jano.Roze@gmail.com>
2018-11-14 21:13:10 -0500

    fixed err

--- client/src/Home.js
+++ client/src/Home.js
@@ -221,18 +221,18 @@ class Home extends Component {
         /*  Scroll Down
          /*----------------------------------------------------*/
     
-        $('.icon-arrow-down').on('click', function () {
-            if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
+        // $('.icon-arrow-down').on('click', function () {
+        //     if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+        //         var target = $(this.hash);
+        //         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        //         if (target.length) {
+        //             $('html,body').animate({
+        //                 scrollTop: target.offset().top
+        //             }, 1000);
+        //             return false;
+        //         }
+        //     }
+        // });
     
     
         //How it works tooltips
