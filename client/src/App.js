b8537d22cb24f38ae19925ef3503966c645821fb
Jroze88 <Jano.Roze@gmail.com>
2018-11-14 21:23:24 -0500

    boardpage

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App.css';
 import Home from "./Home"
 import Play from './play'
+import BoardPage from './Boardpage'
 import { BrowserRouter as Router, Route } from "react-router-dom"
 
 
@@ -13,6 +14,7 @@ class App extends Component {
           <div>
             <Route exact path ="/" component={Home} />
             <Route exact path='/play' component={Play} />
+            <Route exact path='/boardpage' component={BoardPage} />
           </div>
         </Router>
     
---
+++ client/src/Boardpage.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import $ from 'jquery';
+import '/boardpage.css';
+
+
+class BoardPage extends Component {
+
+
+    componentDidMount() {
+        // JAVASCRIPT GOES HERE //
+    }
+
+
+
+
+
+    render() {
+        return(
+            <div>
+            {/* // HTML GOES HERE // */}
+
+
+
+
+
+
+            </div>
+        )
+    }
+
+
+
+}
