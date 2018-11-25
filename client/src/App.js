7b675ad41fda426b8f8acb2cea73d77d883a3630
Jroze88 <Jano.Roze@gmail.com>
2018-11-25 07:13:38 -0500

    resize vids

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Home from "./Home"
 import Play from './play'
 //import BoardPage from './Boardpage'
 import { BrowserRouter as Router, Route } from "react-router-dom"
-import VideoTest from './videotest'
 
 
 
@@ -17,7 +16,6 @@ class App extends Component {
             <Route exact path ="/" component={Home} />
             <Route exact path='/play' component={Play} />
             {/* <Route exact path='/boardpage' component={BoardPage} /> */}
-            <Route exact path = '/videotest' component={VideoTest} />
           </div>
         </Router>
     
