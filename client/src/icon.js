bdddf3318c0d58fdd7e34f53b2dcb94d9ead72cc
Jroze88 <Jano.Roze@gmail.com>
2018-11-28 01:13:35 -0500

    homefix

---
+++ client/src/icon.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import Draggable from 'react-draggable'
+
+
+
+
+
+
+
+
+export default class Icon extends Component {
+
+
+  render() {
+    
+   
+    
+    return (
+
+        <Draggable>
+     <span className='dot' id='LI_1'></span>
+     </Draggable>
+
+
+       )
+      }
+     }
+     
